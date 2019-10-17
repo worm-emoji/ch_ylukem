ec6905bc064b4746a2f79be099779f27077e8b03
Luke Miles <luke@lukemil.es>
2019-10-17 09:08:57 -0700

    Add Square

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Twitter = require("twitter");
 AWS.config.update({ region: "us-west-2" });
 const s3 = new AWS.S3();
 
+let $ = null;
+
 const twitter = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -13,12 +15,12 @@ const twitter = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const amex = $ => {
+const amex = jq => {
   const words = [];
   let startIndex = null;
 
-  $("b").each(function(i, elem) {
-    const text = $(this).text();
+  jq("b").each(function(i, elem) {
+    const text = jq(this).text();
     if (text.indexOf("your Card") > -1) {
       startIndex = i + 1;
     }
@@ -34,14 +36,14 @@ const amex = $ => {
   }
 };
 
-const citibank = $ => {
-  const merchant = $("td")
-    .filter((i, el) => $(el).text() === "Merchant")
+const citibank = jq => {
+  const merchant = jq("td")
+    .filter((i, el) => jq(el).text() === "Merchant")
     .next()
     .text();
 
-  const amount = $("span")
-    .filter((i, el) => $(el).text() === "Amount:")
+  const amount = jq("span")
+    .filter((i, el) => jq(el).text() === "Amount:")
     .closest("tr")
     .text()
     .replace("Amount:", "")
@@ -49,9 +51,23 @@ const citibank = $ => {
   return `${merchant} — ${amount}`;
 };
 
+const square = ($, subject) => {
+  let merchant = null;
+  const matches = subject.match(/(?<=Receipt from).*/);
+  if (matches && matches[0]) {
+    merchant = matches[0].trim();
+  }
+  const amount = $("span")
+    .filter((i, el) => $(el).text() === "$")
+    .parent()
+    .text()
+    .trim();
+  return `${merchant} — ${amount}`;
+};
+
 const main = async message => {
   console.log("Main getting called");
-  const { html } = await simpleParser(message);
+  const { html, subject } = await simpleParser(message);
   $ = cheerio.load(html);
   let response = null;
   if (html.indexOf("American Express") > -1) {
@@ -60,17 +76,24 @@ const main = async message => {
   } else if (html.indexOf("Citibank") > -1) {
     console.log("Citibank");
     response = citibank($);
+  } else if (html.indexOf("Square") > -1) {
+    console.log("Square");
+    response = square($, subject);
   } else {
     console.log("Couldn't determine bank");
   }
   console.log("Response", response);
   if (response !== null) {
-    twitter.post("statuses/update", { status: response }, (err, data) => {
-      if (err) {
-        console.err(err);
-      }
+    return new Promise(resolve => {
+      twitter.post("statuses/update", { status: response }, (err, data) => {
+        if (err) {
+          console.error(err);
+        }
+        resolve(response);
+      });
     });
   }
+  return response;
 };
 
 exports.handler = async event => {
@@ -81,14 +104,14 @@ exports.handler = async event => {
   };
 
   console.log("Message ID", messageId);
-
-  s3.getObject(params, async function(err, data) {
-    if (err) {
-      console.log(err, err.stack);
-    } else {
-      await main(data.Body);
-    }
+  return new Promise(resolve => {
+    s3.getObject(params, async function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        const res = await main(data.Body);
+        resolve(res);
+      }
+    });
   });
-
-  return "";
 };
